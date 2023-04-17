cd688fbc4e5426cb38cedeac97f2a99bb6fbecf3
kaede <kaede3256@gmail.com>
2023-04-17 16:23:12 +0900

    現在地戻れるようにしたいです

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+// import React, { useImperativeHandle, useState } from 'react';
 import GoogleMapReact, { ClickEventValue, Coords } from 'google-map-react';
+import Image from 'next/image';
+import { Box, Text,  Flex } from '@chakra-ui/react';
+import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react';
+
 
 type Props = {
   location: Coords;
   onClick?: () => void;
 };
 
-const Map = (props: Props) => {
-  const { location } = props;
+export type MapRef = {
+  centerMap: (location: Coords) => void;
+};
 
-  // TODO: どこにもどこにも使用していないため不要なら削除してください
+const Map = forwardRef<MapRef, Props>((props, ref) => {
+  const { location } = props;
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [maps, setMaps] = useState<any>(null);
 
-  // 「サウナ ジム」で検索して結果に基づいてマーカーを立てる処理
+  useImperativeHandle(ref, () => ({
+    centerMap: (location: Coords) => {
+      if (map) {
+        map.setCenter(location);
+      }
+    },
+  }));
+
   const handleApiLoaded = ({ map, maps }: { map: google.maps.Map; maps: any }) => {
     setMap(map);
     setMaps(maps);
@@ -22,56 +35,29 @@ const Map = (props: Props) => {
     if (maps === null) return;
     const bounds = new maps.LatLngBounds();
 
-    service.textSearch(
-      {
-        location, // 現在地
-        radius: 1500, // 対象範囲
-        query: 'gym  ジム 温泉 銭湯 サウナ spa',  // 検索ワード
-      },
+    const searchKeywords = ['gym', 'spa', 'ジム', '温泉', '銭湯', 'サウナ'];
 
-      (results, status) => {
-        // 検索に成功したらマーカーを立てる
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          // 検索結果がないなら処理を終了
-          if (!results) return;
+    searchKeywords.forEach((keyword) => {
+      service.textSearch(
+        {
+          location, // 現在地
+          radius: 1500, // 対象範囲
+          query: keyword, // 検索ワード
+        },
 
-          // 検索結果から緯度経度を取得
-          const locationItemList = results.map((result) => {
-            return {
-              lat: result.geometry?.location?.lat(),
-              lng: result.geometry?.location?.lng(),
-              placeId: result.place_id, // Place ID を格納
-            };
-          });
+        (results, status) => {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            if (!results) return;
 
-          locationItemList.forEach((item) => {
-            // Place Details API で店舗の詳細情報を取得
-            const request = {
-              placeId: item.placeId || '',
-              fields: ['name', 'formatted_address', 'formatted_phone_number', 'rating', 'opening_hours', 'website'],
-            };
-            service.getDetails(request, (place, status) => {
-              if (status !== google.maps.places.PlacesServiceStatus.OK) return;
-
-              console.log('店舗名：', place?.name);
-              console.log('詳細：', place?.formatted_address);
-              console.log('電話番号：', place?.formatted_phone_number);
-              console.log('評価：', place?.rating);
-              console.log('営業時間：', place?.opening_hours?.weekday_text);
-              console.log('Web サイト：', place?.website);
+            const locationItemList = results.map((result) => {
+              return {
+                lat: result.geometry?.location?.lat(),
+                lng: result.geometry?.location?.lng(),
+                placeId: result.place_id,
+              };
             });
 
-            // マーカーを作成し、マップ上に表示
-            const marker = new maps.Marker({
-              position: {
-                lat: item.lat,
-                lng: item.lng,
-              },
-              map,
-            });
-            // マーカーにクリックイベントを追加
-            marker.addListener('click', () => {
-              // Place Details API で店舗の詳細情報を取得
+            locationItemList.forEach((item) => {
               const request = {
                 placeId: item.placeId || '',
                 fields: ['name', 'formatted_address', 'formatted_phone_number', 'rating', 'opening_hours', 'website'],
@@ -79,60 +65,96 @@ const Map = (props: Props) => {
               service.getDetails(request, (place, status) => {
                 if (status !== google.maps.places.PlacesServiceStatus.OK) return;
 
-                // 店舗の詳細情報をポップアップ表示
-                const content = `
-                <div>
-                  <h4>${place?.name}</h4>
-                  <p>${place?.formatted_address}</p>
-                  <p>電話番号${place?.formatted_phone_number}</p>
-                  <p>評価${place?.rating}</p>
-                  <p>営業時間${place?.opening_hours?.weekday_text}</p>
-                  <p>ウェブサイト${place?.website}</p>
-                  <a href="https://www.google.com/maps/search/?api=1&query=${place?.name}" target="_blank" rel="noopener noreferrer"  style={{ fontWeight: 'bold', color: 'blue' }}>Google Map で見る</a>
-                </div>
-              `;
-
-                const infoWindow = new google.maps.InfoWindow({
-                  content,
-                });
-                infoWindow.open(map, marker);
+                console.log('店舗名：', place?.name);
+                console.log('詳細：', place?.formatted_address);
+                console.log('電話番号：', place?.formatted_phone_number);
+                console.log('評価：', place?.rating);
+                console.log('営業時間：', place?.opening_hours?.weekday_text);
+                console.log('Web サイト：', place?.website);
               });
-            });
 
-            bounds.extend(marker.position);
-            // marker.addListener('click', () => {
-            //   alert('マーカーがクリックされました');
-            // });
-            bounds.extend(marker.position);
-          });
-          map.fitBounds(bounds);
+              let markerIcon;
+              if (['温泉', '銭湯', 'サウナ'].includes(keyword)) {
+                markerIcon = {
+                  path: google.maps.SymbolPath.CIRCLE,
+                  fillColor: 'blue',
+                  fillOpacity: 1,
+                  strokeWeight: 2,
+                  scale: 8,
+                };
+              } else {
+                markerIcon = undefined;
+              }
+
+              const marker = new maps.Marker({
+                position: {
+                  lat: item.lat,
+                  lng: item.lng,
+                },
+                map,
+                icon: markerIcon,
+              });
+
+              marker.addListener('click', () => {
+                const request = {
+                  placeId: item.placeId || '',
+                  fields: ['name', 'formatted_address', 'formatted_phone_number', 'rating', 'opening_hours', 'website'],
+                };
+                service.getDetails(request, (place, status) => {
+                  if (status !== google.maps.places.PlacesServiceStatus.OK) return;
+
+                  const content = `
+                  <div>
+                    <h4>${place?.name}</h4>
+                    <p>${place?.formatted_address}</p>
+                    <p>電話番号：${place?.formatted_phone_number}</p>
+                    <p>評価：${place?.rating}</p>
+                    <p>営業時間：${place?.opening_hours?.weekday_text}</p>
+                    <p>ウェブサイト：${place?.website}</p>
+                    <a href="https://www.google.com/maps/search/?api=1&query=${place?.name}" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 'bold', color: 'blue' }}>Google Map で見る</a>
+                  </div>
+                `;
+
+                  const infoWindow = new google.maps.InfoWindow({
+                    content,
+                  });
+                  infoWindow.open(map, marker);
+                });
+              });
+
+              bounds.extend(marker.position);
+              map.fitBounds(bounds);
+            });
+          }
         }
-      }
-    );
+      );
+    });
   };
 
 
-  function handleMapClick(value: ClickEventValue) {
+  const handleMapClick = (value: ClickEventValue) => {
     if (props.onClick) {
       props.onClick();
     }
-  }
+  };
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>
+      
       <GoogleMapReact
         bootstrapURLKeys={{
-          // 自分のAPIキーを環境変数に設定して使用してください
           key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API as string,
           libraries: ['drawing', 'geometry', 'places', 'visualization'],
         }}
         defaultCenter={location}
-        defaultZoom={5.09} onClick={handleMapClick}
+        defaultZoom={5.09}
+        onClick={handleMapClick}
         yesIWantToUseGoogleMapApiInternals
         onGoogleApiLoaded={handleApiLoaded}
       />
+
     </div>
   );
-};
+});
 
 export default Map;
--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -1,44 +1,104 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Map from './Map';
-import { Button } from '@chakra-ui/react';
-
+import { Box, Button, Flex, Text, Image } from '@chakra-ui/react';
+import { MapRef } from './Map';
 
 export const MapContainer = () => {
   const [location, setLocation] = useState<google.maps.LatLngLiteral | null>(null);
 
+
   useEffect(() => {
+    let watchId: number;
+
     const searchNearbyPlaces = async () => {
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          setLocation({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        }, null, { enableHighAccuracy: true });
+        // 監視を開始し、ユーザーの位置が変更されたら位置情報を更新する
+        watchId = navigator.geolocation.watchPosition(
+          async (position) => {
+            setLocation({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          null,
+          { enableHighAccuracy: true }
+        );
       } else {
         alert('位置情報の許可してください.');
       }
     };
     searchNearbyPlaces();
+
+    // クリーンアップ時に位置情報の監視を停止する
+    return () => {
+      if (watchId) {
+        navigator.geolocation.clearWatch(watchId);
+      }
+    };
   }, []);
-  
 
   const defaultLatLng = {
     lat: 35.7022589,
     lng: 129.7744733,
   };
 
-  
+  const mapRef = useRef<MapRef | null>(null);
+
+  const goToCurrentLocation = () => {
+    if (location && mapRef.current) {
+      mapRef.current.centerMap(location);
+    }
+  };
 
   return (
     <>
-      
-      
-      <Map location={location ?? defaultLatLng}/>
+      <Flex justifyContent="center" alignItems="center" bgColor="#6C9F43" textAlign="center" flexDirection="row">
+        <Box>
+          <Text
+            fontFamily="游ゴシック, YuGothic, sans-serif"
+            color="white"
+            pb={"10px"}
+          >
+            <Image src="/images/marker.png" width={30} height={30} alt="ジム" />がジム
+          </Text>
+        </Box>
+        <Box>
+          <Text
+            fontFamily="游ゴシック, YuGothic, sans-serif"
+            color="white"
+            ml={"10px"}
+          >
+            <Box as="span" color="blue" fontWeight="bold">
+              〇
+            </Box>
+            が温泉・銭湯・サウナ
+          </Text>
+        </Box>
+      </Flex>
+
+      <Box
+        textAlign="center"
+        bgColor="#6C9F43">
+        <Button
+          onClick={goToCurrentLocation} 
+          borderRadius="50px"
+          px={20}
+          py={10}
+          bg="#38B6FF"
+          color="white"
+          fontFamily="游ゴシック, YuGothic, sans-serif"
+          cursor="pointer"
+          _hover={{ cursor: "pointer" }}
+          width="100px"
+          height={"15px"}
+          margin={"5px"}
+        >
+          現在地に戻る
+        </Button>
+      </Box>
+      <Map location={location ?? defaultLatLng} />
     </>
   );
 };
-function setMap(arg0: (prevMap: any) => any) {
-  throw new Error('Function not implemented.');
-}
+
 
