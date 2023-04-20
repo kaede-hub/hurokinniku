52db5d5f401bfaade16fb823e0a7e6140fe10523
kaede <kaede3256@gmail.com>
2023-04-20 12:15:38 +0900

    うまくいかないので、再度やり直し

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,10 @@ export default function Document() {
     <Html lang="en">
       
       <Head />
-      {/* Safariではasync属性がうまく機能しないため、Google Maps JavaScript APIの読み込みをasyncでなくdeferで行う */}
       <script
-        defer
-        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
-      />
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
+            async
+          />
       <body>
         <Main />
         <NextScript />
