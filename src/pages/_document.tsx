b7653daf8dede93c50e3a244325a69f04e104115
kaede <kaede3256@gmail.com>
2023-04-20 12:34:08 +0900

    修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ export default function Document() {
       <script
             src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
             async
+            defer
+            crossOrigin="anonymous"
           />
       <body>
         <Main />
